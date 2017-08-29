5c289f5eb7fbf2119ac2176ad69c85e807bfe46e
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-08-29 18:12:51 -0300

    Table components

---
+++ src/components/table/index.js
@@ -0,0 +1,81 @@
+import React from "react";
+import Proptypes from "prop-types";
+
+export const TR = ({ children }) =>
+  <tr>
+    {children}
+  </tr>;
+
+TR.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+export const TBODY = ({ children }) =>
+  <tbody>
+    {children}
+  </tbody>;
+
+TBODY.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+export const TD = ({ children }) =>
+  <td>
+    {children}
+  </td>;
+
+TD.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+export const THEAD = ({ children }) =>
+  <thead>
+    <tr>
+      {children}
+    </tr>
+  </thead>;
+
+THEAD.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+export const TH = ({ children }) =>
+  <th>
+    {children}
+  </th>;
+
+TH.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+const Table = ({ children }) => {
+  return (
+    <table class="table table-striped table-hover ">
+      {children}
+    </table>
+  );
+};
+
+Table.propTypes = {
+  children: Proptypes.oneOfType([
+    Proptypes.node,
+    Proptypes.arrayOf(Proptypes.node)
+  ])
+};
+
+export default Table;
