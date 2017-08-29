156e8d12a0b94bca6c3613f62044e0b8ecd870ec
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-08-29 18:13:48 -0300

    ModelTable component

---
+++ src/components/modelTable/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Table, { TBODY, TD, THEAD, TH, TR } from "../table";
+import { filterObjectFields, capitalizeFirstLetter } from "../../lib/helpers";
+
+const ModelRow = model =>
+  <TR>
+    {Object.keys(model).map(field =>
+      <TR>
+        {model[field]}
+      </TR>
+    )}
+  </TR>;
+
+ModelRow.propTypes = {
+  model: PropTypes.object.isRequired
+};
+
+const ModelTable = ({ models, fields }) =>
+  <Table>
+    <THEAD>
+      {fields.map(field =>
+        <TH key={field}>
+          {capitalizeFirstLetter(field)}
+        </TH>
+      )}
+      <TH />
+    </THEAD>
+    <TBODY>
+      {models.map(model => <ModelRow model={filterObjectFields(model)} />)}
+    </TBODY>
+  </Table>;
+
+ModelTable.propTypes = {
+  models: PropTypes.arrayOf(PropTypes.object).isRequired,
+  fields: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default ModelTable;
