18c38e8bcbbd3bf9b11d7564a81b02c79a7f2d4b
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-08-31 01:44:06 -0300

    added work reducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import counter from "./counter";
 import auth from "./auth";
+import work from "./work";
 
 export default combineReducers({
   counter,
-  auth
+  auth,
+  work
 });
