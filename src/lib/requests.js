1efe4f4df12591e7cb1773d6ca54b7a05c2166ef
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-08-31 01:43:56 -0300

    fixed request GET bug

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,20 +1,26 @@
+import { transformObjectInUrl } from "./helpers";
+
 const options = (method, data) => {
   const { token } = localStorage;
   const Authorization = token ? { Authorization: `Bearer ${token}` } : {};
+  const body = data ? { body: JSON.stringify(data) } : {};
   return {
     method,
     headers: {
       "Content-Type": "application/json",
       ...Authorization
     },
-    body: JSON.stringify(data)
+    ...body
   };
 };
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 export function GET(url, data) {
-  return fetch(`${API_URL}/${url}`, options("get", data)).then(x => x.json());
+  return fetch(
+    `${API_URL}/${url}${data ? transformObjectInUrl(data) : ""}`,
+    options("get")
+  ).then(x => x.json());
 }
 
 export function POST(url, data) {
